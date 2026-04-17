deeffa0e5b9acbc060a888e8ce7cf686bf65fa16
satishlakamsani <lakamsanisatish@gmail.com>
2026-04-17 14:08:54 +0530

    added sgrules

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -6,4 +6,7 @@ locals {
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
   ingress_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
+  eks_control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
+  eks_node_sg_id = data.aws_ssm_parameter.eks_node_sg_id.value
+  openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
 }

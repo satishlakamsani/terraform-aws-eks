4db4994b40732893286c3d58922c67998f580fe8
satishlakamsani <lakamsanisatish@gmail.com>
2026-04-18 09:21:08 +0530

    added sgrules

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -28,3 +28,11 @@ resource "aws_ssm_parameter" "database_subnet_ids"{
   value = join(",",module.vpc.database_subnet_ids)
 
 }
+
+resource "aws_ssm_parameter" "database_subnet_group_name"{
+
+  name = "/${var.project}/${var.environment}/database_subnet_group_name"
+  type = "String"
+  value = module.vpc.database_subnet_group_name
+
+}
